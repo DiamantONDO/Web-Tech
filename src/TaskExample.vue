c365a0486b765834eb7e24f02169b688b42c998e
ONDO ABESSOLO Diamant Anthony <anthodidiams@gmail.com>
2026-02-12 20:31:58 -0800

    Adding required files to complete my following task

---
+++ src/TaskExample.vue
@@ -0,0 +1,22 @@
+<script setup="tsx">
+import TaskCrud from './TaskCrud.vue';
+</script>
+
+<template>
+
+  <header></header>
+  <main>
+    <TaskCrud/>
+  </main>
+  <footer></footer>
+  
+  <header>
+  </header>
+  
+
+  <footer>
+    </footer>
+</template>
+
+<style scoped>
+</style>
